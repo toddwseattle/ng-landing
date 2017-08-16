2c6770970ede2988a9a1f010169c032062c6d27d
Todd Warren <todd@warrenfamily.org>
2017-08-16 15:47:51 -0700

    make grid work with background image and foreground text on hover
    hook some window events for the ts and added css to make work.  Added a few divs.

--- src/app/activity-grid/activity-grid.component.ts
+++ src/app/activity-grid/activity-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { IActivity, Activity, InvestmentActivity, IImage } from '../common/activity';
 import { divergentinvestments } from '../common/activity-data';
 
@@ -19,10 +19,10 @@ interface ICard {
 })
 
 export class ActivityGridComponent implements OnInit {
-
+  cols = 3;
   items: ICard[]= [];
   actvites: IActivity[];
-  constructor() {
+  constructor(public el: ElementRef) {
     this.actvites = divergentinvestments;
     this.actvites.forEach(activity => {
        this.items.push({header: activity.name,
@@ -33,6 +33,16 @@ export class ActivityGridComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.cols =this.sizeCols(this.el.nativeElement.offsetWdith);
+  }
+  sizeCols(width) {
+    return( Math.floor(width / 300) >= 1 ? Math.floor(width / 300) : 1);
+  }
+  onResize(event) {
+    const element = event.target.innerWidth;
+    console.log(element);
+    this.cols = this.sizeCols(element);
+    console.log('columngs %s', this.cols);
   }
 
 }
