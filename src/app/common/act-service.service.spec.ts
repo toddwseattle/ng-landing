862401bba453e082a62a5a9606e84e801810abf3
Todd Warren <todd@warrenfamily.org>
2017-08-18 19:29:20 -0700

    Add getactivities() member and support Investments
    Added getactivities without paramaters whcih returns all activities and also added support for getactivities([ACTIVETYPE.Investment]) to
    return only Investment Activities.
    
    Incroporated simple unit tests for both.

--- src/app/common/act-service.service.spec.ts
+++ src/app/common/act-service.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ActServiceService } from './act-service.service';
+import { ACTIVETYPE, Activity } from './activity';
+import { Observable } from 'rxjs/observable';
 
 describe('ActServiceService', () => {
   beforeEach(() => {
@@ -11,5 +13,22 @@ describe('ActServiceService', () => {
 
   it('should be created', inject([ActServiceService], (service: ActServiceService) => {
     expect(service).toBeTruthy();
+  }));
+  it('should have a getactivities method', inject([ActServiceService], (service: ActServiceService) => {
+    expect(service.getactivities).toBeTruthy();
+  }));
+   it('getactivities should return activities without parameters', inject([ActServiceService], (service: ActServiceService, done) => {
+    const actobs = service.getactivities();
+    actobs.subscribe((actives) => {
+      expect(actives.length).toBeGreaterThan(0);
+    });
+  }));
+  it('getactivities([ACTIVETYPE.Invesment]) should return one or more investment activities',
+    inject([ActServiceService], (service: ActServiceService, done) => {
+      const actobs = service.getactivities([ACTIVETYPE.Investment]);
+      actobs.subscribe((actives) => {
+        expect(actives.length).toBeGreaterThan(0);
+        expect(actives.every(v => (v.activetype === ACTIVETYPE.Investment)));
+      });
   }));
 });
--- src/app/common/act-service.service.ts
+++ src/app/common/act-service.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Activity, IActivity, ACTIVETYPE  } from './activity';
+import { divergentinvestments } from './activity-data';
+import { Observable } from 'rxjs/observable';
 
 @Injectable()
 export class ActServiceService {
 
   constructor() { }
 
+  public getactivities(activities?: ACTIVETYPE[]): Observable<Activity[]> {
+    if ( activities == null ) {
+      return Observable.create(obs => {
+        obs.next(divergentinvestments);
+        obs.complete();
+      });
+    } else if (activities.indexOf(ACTIVETYPE.Investment) >= 0) {
+      return Observable.create(obs => {
+        obs.next(divergentinvestments);
+        obs.complete();
+      });
+    }
+  }
 }
