652a789fb28bf838768aea7cbd2a88d8ab8b1501
Todd Warren <todd@warrenfamily.org>
2017-10-09 06:12:50 -0700

    Add new files to make last commit work
    files added for the input file and a generic image
    	new file:   src/app/byte-format.pipe.spec.ts
    	new file:   src/app/byte-format.pipe.ts
    	new file:   src/app/input-file/input-file.component.html
    	new file:   src/app/input-file/input-file.component.scss
    	new file:   src/app/input-file/input-file.component.spec.ts
    	new file:   src/app/input-file/input-file.component.ts
    	new file:   src/assets/images/480px-Image-x-generic-2.svg.png

---
+++ src/app/input-file/input-file.component.ts
@@ -0,0 +1,110 @@
+import { Component, OnInit, Input, ViewChild, ElementRef, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-file',
+  templateUrl: './input-file.component.html',
+  styleUrls: ['./input-file.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputFileComponent),
+    multi: true
+  }]
+})
+export class InputFileComponent implements OnInit, ControlValueAccessor {
+
+  @Input() disabled: boolean;
+  @Input() placeholder: string;
+  @Input() valuePlaceholder: string;
+  @Input() required: boolean;
+  @Input() multiple: boolean;
+  focused: boolean;
+  fileNames: string;
+
+  private _files: File[];
+
+  @ViewChild('input') rawInput: ElementRef;
+
+  /**
+   * https://tympanus.net/codrops/2015/09/15/styling-customizing-file-inputs-smart-way/
+   * https://blog.thoughtram.io/angular/2016/07/27/custom-form-controls-in-angular-2.html
+   */
+  constructor() { }
+
+  private _propagateChange = (_: any) => { };
+  private _propagateTouched = (_: any) => { };
+
+  writeValue(obj: any): void {
+    this.files = obj;
+  }
+  registerOnChange(fn: any): void {
+    this._propagateChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this._propagateTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  get input() {
+    return this.rawInput.nativeElement;
+  }
+
+  get hasFiles(): boolean {
+    return this.files !== undefined && this.files.length > 0;
+  }
+
+  get files(): File[] {
+    return this._files;
+  }
+
+  set files(fileList) {
+    let fileArray: File[];
+    if (fileList) {
+      fileArray = [];
+      for (let i = 0; i < fileList.length; i++) {
+        fileArray.push(fileList[i]);
+      }
+      this._propagateChange(fileArray);
+    }
+    this._files = fileArray;
+    this._initFileNames();
+  }
+
+  ngOnInit() {
+    this.disabled = this.disabled !== undefined ? this.disabled : false;
+    this.required = this.required !== undefined ? this.required : false;
+    this.multiple = this.multiple !== undefined ? this.multiple : false;
+    this.focused = false;
+    this._initFileNames();
+  }
+
+  onFocusToggle() {
+    this.focused = !this.focused;
+  }
+
+  onTouched(event) {
+    this._propagateTouched(null);
+  }
+
+  onChange(event) {
+    this.files = this.input.files;
+  }
+
+  open() {
+    if (!this.disabled) {
+      this.input.click();
+    }
+  }
+
+  private _initFileNames() {
+    let str = [this.valuePlaceholder || 'Aucun fichier sélectionné'];
+    if (this.hasFiles) {
+      str = this.files.map(f => f.name);
+    }
+    this.fileNames = str.join(', ');
+  }
+
+}
